bc24f7e26c782e52ccd1458d7fa0214ee947da82
Carolina Labastida Meza <carolina.labastida@nectri.com>
2023-04-11 19:13:23 -0600

    Sidenav, toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MaterialModule } from './modules/material/material.module';
+import { IndexComponent } from './pages/Student/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    ToolbarComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
